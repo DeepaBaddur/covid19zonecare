c8a210a169171f59eb4829f089a928eb3204b419
Baddur <858828@cognizant.com>
2020-04-15 15:33:41 +0530

    Revomed duplicate files

--- restful-web-services/src/main/java/com/wardinfo/rest/webservices/restfulwebservices/Repositories/PincodeDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wardinfo.rest.webservices.restfulwebservices.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wardinfo.rest.webservices.restfulwebservices.Entity.Pincode;
-
-public interface PincodeDataRepository extends JpaRepository<Pincode, Integer> {
-
-}
--- restful-web-services/src/main/java/com/wardinfo/rest/webservices/restfulwebservices/Repositories/WardServicesDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wardinfo.rest.webservices.restfulwebservices.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wardinfo.rest.webservices.restfulwebservices.Entity.WardServices;
-
-public interface WardServicesDataRepository extends JpaRepository<WardServices, Integer> {
-
-}
