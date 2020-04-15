8d0c826b744cb0d9337c9c0a8ee4a0014d6a41d1
Baddur <858828@cognizant.com>
2020-04-15 15:11:52 +0530

    Removed unnecessary code

--- restful-web-services/src/main/java/com/wardinfo/rest/webservices/restfulwebservices/Repositories/WardDetailsDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wardinfo.rest.webservices.restfulwebservices.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wardinfo.rest.webservices.restfulwebservices.Entity.WardDetails;
-
-public interface WardDetailsDataRepository extends JpaRepository<WardDetails, Integer> {
-
-}
